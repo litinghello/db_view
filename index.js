5e0f8c68d5fde339065ef249263b9ae5f133a141
litinghello <2863515010@qq.com>
2020-09-24 19:00:17 +0800

    update

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const ipcMain = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');//自动播放开启
 
+if(load_config_file(app) === false){
+  return 0;
+}
+
 app.on('ready',function(){//当 Electron 完成初始化时被触发。
     const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
     let window = new BrowserWindow({width:width,height:height,minWidth:width/4,minHeight:height/4,fullscreen:false,autoHideMenuBar:true,useContentSize:true,webPreferences:{devTools:true,nodeIntegration:true,nodeIntegrationInWorker:true}});
@@ -16,11 +20,32 @@ app.on('ready',function(){//当 Electron 完成初始化时被触发。
     });
 });
 
+function load_config_file(app){
+  const fs = require("fs");
+  const {execSync} = require('child_process');
+  let exe_path = app.getPath("exe");
+  exe_path = exe_path.substr(0,exe_path.lastIndexOf("\\"));//获取可执行文件目录
+  let conf = {userData:exe_path+"\\userData",machine_id:""};
+  let conf_path = exe_path+"\\soft_config";
+  let machine_id = require('node-machine-id').machineIdSync({original: true});
+  if (fs.existsSync(conf_path)) {
+      let read_config = JSON.parse(fs.readFileSync(conf_path));//读取配置信息
+      if(machine_id !== read_config.machine_id){
+        require('electron').dialog.showErrorBox('警告',`请删除软件，清除所有数据重试！机器码：${machine_id}`);
+        console.log("error code:0001");
+        return false;
+      }
+      conf.userData = read_config.userData;
+  }else{
+    conf.machine_id = machine_id;
+    fs.writeFileSync(conf_path,JSON.stringify(conf));//写入配置信息
+  }
+  app.setPath("userData",conf.userData);//修改用户数据目录
+  return true;
+}
 app.on('window-all-closed',function(){//当所有的窗口都被关闭时触发。
 	app.quit();
 });
-
-
 
 ipcMain.on('download', (event, arg) => {
 	//console.log(arg);
